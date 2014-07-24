90af4fc4a93eacb7591d8c544eabdc10f847a135
roman <roman.kellner@xtradesoft.com>
2014-07-24 16:07:18 +0200

    Improve handling of multiple parameters

--- src/main/java/com/xtradesoft/dlp/impl/http/HttpRequestHandler.java
+++ src/main/java/com/xtradesoft/dlp/impl/http/HttpRequestHandler.java
@@ -7,8 +7,11 @@ package com.xtradesoft.dlp.impl.http;
 import java.io.IOException;
 import java.net.URI;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
+import org.apache.http.NameValuePair;
+import org.apache.http.client.utils.URLEncodedUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -20,60 +23,57 @@ import com.sun.net.httpserver.HttpHandler;
  */
 public class HttpRequestHandler implements HttpHandler {
 
-    /** The Constant LOGGER. */
-    private static final Logger LOGGER = LoggerFactory.getLogger(HttpRequestHandler.class);
+  /** The Constant LOGGER. */
+  private static final Logger LOGGER = LoggerFactory.getLogger(HttpRequestHandler.class);
 
-    /** The observer. */
-    private HttpObserver observer;
+  /** The observer. */
+  private HttpObserver observer;
 
-    /*
-     * (non-Javadoc)
-     * @see
-     * com.sun.net.httpserver.HttpHandler#handle(com.sun.net.httpserver.HttpExchange
-     * )
-     */
-    @Override
-    public void handle(HttpExchange exchange) throws IOException {
+  /*
+   * (non-Javadoc)
+   * @see
+   * com.sun.net.httpserver.HttpHandler#handle(com.sun.net.httpserver.HttpExchange
+   * )
+   */
+  @Override
+  public void handle(HttpExchange exchange) throws IOException {
 
-        LOGGER.info("recieved, http request type: {}, uri: {}", exchange.getRequestMethod(), exchange.getRequestURI());
-        final Map<String, String> parameters = parseQueryParameters(exchange.getRequestURI());
-        final boolean success = observer.notify(parameters);
-        exchange.sendResponseHeaders(success ? 200 : 400, 0);
-        exchange.getResponseBody().close();
-        exchange.close();
-        LOGGER.info("sent, http response: {}, request uri: {}", success ? 200 : 400, exchange.getRequestURI());
+    LOGGER.info("recieved, http request type: {}, uri: {}", exchange.getRequestMethod(), exchange.getRequestURI());
+    final Map<String, String> parameters = parseQueryParameters(exchange.getRequestURI());
+    final boolean success = observer.notify(parameters);
+    exchange.sendResponseHeaders(success ? 200 : 400, 0);
+    exchange.getResponseBody().close();
+    exchange.close();
+    LOGGER.info("sent, http response: {}, request uri: {}", success ? 200 : 400, exchange.getRequestURI());
+  }
+
+  /**
+   * Parses the query parameters.
+   *
+   * @param uri
+   *            the uri
+   * @return the map
+   */
+  protected Map<String, String> parseQueryParameters(final URI uri) {
+
+    List<NameValuePair> params = URLEncodedUtils.parse(uri, "UTF-8");
+    final Map<String, String> map = new HashMap<String, String>();
+    for (NameValuePair param : params) {
+      LOGGER.debug("http query parameter, {}: {}", param.getName(), param.getValue());
+      map.put(param.getName(), param.getValue());
     }
+    return map;
+  }
 
-    /**
-     * Parses the query parameters.
-     * 
-     * @param uri
-     *            the uri
-     * @return the map
-     */
-    protected Map<String, String> parseQueryParameters(final URI uri) {
+  /**
+   * Register.
+   *
+   * @param observer
+   *            the observer
+   */
+  void register(HttpObserver observer) {
 
-        final Map<String, String> map = new HashMap<String, String>();
-        if (null != uri.getQuery() && uri.getQuery().length() > 0) {
-          final String[] params = uri.getQuery().split("&");
-            for (final String param : params) {
-                final String[] pair = param.split("=", 2);
-                LOGGER.debug("http query parameter, {}: {}", pair[0], pair[1]);
-                map.put(pair[0], pair.length > 1 ? pair[1] : null);
-            }
-        }
-        return map;
-    }
-
-    /**
-     * Register.
-     * 
-     * @param observer
-     *            the observer
-     */
-    void register(HttpObserver observer) {
-
-        this.observer = observer;
-    }
+    this.observer = observer;
+  }
 
 }
